0bd965319f7a673a51d6aca0803a30218ebf1cb3
Yuri Mello <contato@yurimello.com>
2019-02-27 22:29:44 -0300

    criando componente de lista de consultas

---
+++ src/components/Consultas/Consultas.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export class Consultas extends Component {
+
+constructor(props){
+    super(props);
+    this.state = {
+        
+    }
+}
+
+
+
+
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Consultas
