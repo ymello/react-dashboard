5fe5ffc2dbf4fdab7767216823ea3661f8b6a2ef
Yuri Mello <contato@yurimello.com>
2019-02-27 22:33:00 -0300

    adicionando endpoint

--- src/components/Consultas/Consultas.js
+++ src/components/Consultas/Consultas.js
@@ -5,19 +5,21 @@ export class Consultas extends Component {
 constructor(props){
     super(props);
     this.state = {
-        
+        realizadas : [],
+        marcadas: []
     }
 }
 
 
-
-
+componentDidMount(){
+    fetch("http://www.devup.com.br/php/api-dashboard/api/consultas")
+    .then(resultado =>resultado.json().then(dados => this.setState(dados)));
+  }
 
 
   render() {
     return (
       <div>
-        
       </div>
     )
   }
